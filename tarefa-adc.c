be783b7e55f0c28515d6a43db8f846bbf5dd10af
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-02-12 23:07:30 -0300

    Primeiro commit com a estrutura básica da tarefa

---
+++ tarefa-adc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
+#include "hardware/clocks.h"
+
+int main()
+{
+    while(true)
+    {
+
+    }
+}
