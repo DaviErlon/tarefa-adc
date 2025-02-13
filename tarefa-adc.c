031e3c67cd5d516e82b44ef8c426667d0129384d
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-02-13 00:52:39 -0300

    adicionei o código dos leds, isto é, fiz as interrupções, fiz o código do adc e do pwm. Testei e está funcionando perfeitamente.

--- tarefa-adc.c
+++ tarefa-adc.c
@@ -5,11 +5,102 @@
 #include "inc/ssd1306.h"
 #include "inc/font.h"
 #include "hardware/clocks.h"
+#include "hardware/pwm.h"
+#include "hardware/adc.h"
+
+#define BOTAO_A 5
+#define BOTAO_JOY 22
+#define JOY_Y 26
+#define JOY_X 27
+#define LED_R 13
+#define LED_G 11
+#define LED_B 12
+
+static uint slice = 0;
+static volatile bool flag = true;
+
+void callback(uint gpio, uint32_t events)
+{
+    static uint32_t tempo_antes = 0;
+    uint32_t tempo_agora = to_ms_since_boot(get_absolute_time());
+
+    if (tempo_agora - tempo_antes > 200)
+    {   
+        if(gpio == BOTAO_A)
+        {
+            flag = !flag;
+            pwm_set_enabled(slice, flag);
+        } 
+        else
+        {
+            gpio_get(LED_G) ? gpio_put(LED_G, false) : gpio_put(LED_G, true);
+        }
+        tempo_antes = tempo_agora;
+    }
+    gpio_acknowledge_irq(gpio, events);
+}
+
+void gpio_configuracao()
+{
+    gpio_init(BOTAO_A);
+    gpio_init(BOTAO_JOY);
+    gpio_init(LED_R);
+    gpio_init(LED_G);
+    gpio_init(LED_B);
+    gpio_set_dir(BOTAO_A, GPIO_IN);
+    gpio_set_dir(BOTAO_JOY, GPIO_IN);
+    gpio_set_dir(LED_R, GPIO_OUT);
+    gpio_set_dir(LED_G, GPIO_OUT);
+    gpio_set_dir(LED_B, GPIO_OUT);
+    gpio_pull_up(BOTAO_A);
+    gpio_pull_up(BOTAO_JOY);
+}
+
+void pwm_configuracao()
+{
+    gpio_set_function(LED_R, GPIO_FUNC_PWM);
+    gpio_set_function(LED_B, GPIO_FUNC_PWM);
+
+    slice = pwm_gpio_to_slice_num(LED_R); // mesmo slice do LED_B
+
+    pwm_set_clkdiv(slice, 125.0);
+    pwm_set_wrap(slice, 8196);
+    pwm_set_gpio_level(LED_R, 0);
+    pwm_set_gpio_level(LED_B, 0);
+    pwm_set_enabled(slice, true);  
+}
+
+void adc_configuracao()
+{
+    adc_init();
+    adc_gpio_init(JOY_Y);
+    adc_gpio_init(JOY_X);
+    adc_set_round_robin(0x03);
+}
 
 int main()
-{
+{   
+    gpio_configuracao();
+    pwm_configuracao();
+    adc_configuracao();
+
+    gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, callback);
+    gpio_set_irq_enabled_with_callback(BOTAO_JOY, GPIO_IRQ_EDGE_FALL, true, callback);
+
+    int16_t v_y;
+    int16_t v_x;
     while(true)
     {
+        if(flag)
+        {
+            v_y = adc_read();
+            v_x = adc_read();
+            v_y = ( abs(v_y - 2048) ) * 4;
+            v_x = ( abs(v_x - 2048) ) * 4;
+            pwm_set_gpio_level(LED_B, v_y);
+            pwm_set_gpio_level(LED_R, v_x);
+        }
 
+        sleep_ms(10);
     }
 }
